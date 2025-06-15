080f37ba3e361c1d835458e2ced3c822bfec563f
Lilian Pham <lilianlienpham@gmail.com>
2025-06-15 02:00:22 -0700

    edited bio paragraph and edited style changes

--- src/pages/main-page/main.jsx
+++ src/pages/main-page/main.jsx
@@ -9,7 +9,13 @@ function MainPage() {
         <div className="main-page">
   
           <div className="text-content">
-            
+          <h1>Lilian Pham</h1>
+            <h2>Overview:</h2>
+            <p>Hello! My name is Lilian and I am a second-year Computer Science student at Simon Fraser University
+              with a passion for using technology to make a meaningful impact. Outside of the tech space, I’ve spent years
+            leading community-based youth initiatives focused on STEM education and accessibility for kids.
+            These experiences have shaped how I approach problem-solving, communication, and collaboration.
+            </p> 
 
         <div className="nav-links">
           <a
