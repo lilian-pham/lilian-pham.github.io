92794f46db88688ebcca3464b340a353a003302b
Lilian Pham <lilianlienpham@gmail.com>
2025-06-15 15:32:30 -0700

    added page navigation to the navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
 import MainPage from './pages/main-page/main';
-import Experience from './pages/experience-page/experience-page';
 import Navbar from "./components/navBar";
-import ProjectsPage from './pages/project-page/project-page';
 import EducationPage from './pages/education-page/education-page';
+import ExperiencePage from './pages/experience-page/experience-page';
+import ProjectsPage from './pages/project-page/project-page';
 
 function App() {
-
   return (
-    <div className="App">
-      <Navbar/>
-      <MainPage />
-      <Experience />
-      <ProjectsPage/>
-      <EducationPage/>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainPage/>} />
+        <Route path="/education" element={<EducationPage/>} />
+        <Route path="/experience" element={<ExperiencePage/>} />
+        <Route path="/projects" element={<ProjectsPage/>} />
+      </Routes>
+    </Router>
   );
 }
 
