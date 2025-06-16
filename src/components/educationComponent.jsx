1e87d5c4cfac94800ad05c3116c24655be5f3800
Lilian Pham <lilianlienpham@gmail.com>
2025-06-15 23:09:58 -0700

    styling changes to education title page

--- src/components/educationComponent.jsx
+++ src/components/educationComponent.jsx
@@ -4,40 +4,43 @@ import '../styling/education.css';
 
 const EducationComponent = () => {
     return (
-      <div className="education">
-        <div>
-          <img src={sfu} alt="Simon Fraser University logo" className="education-img" />
-          <h3 className="education-title">Simon Fraser University</h3>
-          <span className="education-description">Expected Graduation: May 2027</span>
+        <div className="education">
+            <div>
+                <img src={sfu} alt="Simon Fraser University logo" className="education-img" />
+            </div>
+            <div className="awards-container">
+                <div>
+                    <p className="study-description">
+                        <strong>Bachelor of Science in Computer Science</strong>
+                    </p>
+                </div>
+                <div>
+                    <p className="education-description">
+                        <strong>Schulich Leader Scholarship of Canada</strong>
+                    </p>
+                    <p className="subdescription">
+                        Awarded Canada’s most prestigious STEM scholarship valued at <strong>$100,000</strong> for academic excellence, leadership, and entrepreneurship.
+                    </p>
+                </div>
+                <div>
+                    <p className="education-description">
+                        <strong>Rajan Family Scholarship in Computing Science</strong>
+                    </p>
+                    <p className="subdescription">
+                        Offered a <strong>$40,000</strong> scholarship as the top female candidate entering SFU’s Computer Science program.
+                    </p>
+                </div>
+                <div>
+                    <p className="education-description">
+                        <strong>Governor General Bronze Medal of Canada</strong>
+                    </p>
+                    <p className="subdescription">
+                        Recognized as the top graduating student at the bronze level (99% average).
+                    </p>
+                </div>
+            </div>
         </div>
-        <div className="awards-container">
-          <div>
-            <p className="education-description">
-              <strong>Schulich Leader Scholarship of Canada</strong>
-            </p>
-            <p className="subdescription">
-              Awarded Canada’s most prestigious STEM scholarship valued at <strong>$100,000</strong> for academic excellence, leadership, and entrepreneurship.
-            </p>
-          </div>
-          <div>
-            <p className="education-description">
-              <strong>Rajan Family Scholarship in Computing Science</strong>
-            </p>
-            <p className="subdescription">
-              Offered a <strong>$40,000</strong> scholarship as the top female candidate entering SFU’s Computer Science program.
-            </p>
-          </div>
-          <div>
-            <p className="education-description">
-              <strong>Governor General Bronze Medal of Canada</strong>
-            </p>
-            <p className="subdescription">
-              Recognized as the top graduating student at the bronze level (99% average).
-            </p>
-          </div>
-        </div>
-      </div>
     );
-  };
-  
-  export default EducationComponent;
+};
+
+export default EducationComponent;
