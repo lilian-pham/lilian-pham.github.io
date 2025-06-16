a6085aca3b1ff2e0737e25bd25a875b4e1bee280
Lilian Pham <lilianlienpham@gmail.com>
2025-06-15 23:41:56 -0700

    adding hyperlinks to external scholarship sites

--- src/components/educationComponent.jsx
+++ src/components/educationComponent.jsx
@@ -3,6 +3,11 @@ import sfu from "../images/sfu.webp"
 import '../styling/education.css';
 
 const EducationComponent = () => {
+    const sfuLink = "https://www.sfu.ca/fas/computing.html";
+    const schulichLink = "https://schulichleaders.com/";
+    const rajanLink = "https://www.sfu.ca/students/financial-aid/entrance/regulations/rajan-family.html";
+    const gradMedal = "https://stpats.bc.ca";
+
     return (
         <div className="education">
             <div>
@@ -11,12 +16,18 @@ const EducationComponent = () => {
             <div className="awards-container">
                 <div>
                     <p className="study-description">
-                        <strong>Bachelor of Science in Computer Science</strong>
+                    <a href={sfuLink} target="_blank" rel="noopener noreferrer" className="sfu-cs-link">
+                            <strong>SFU - Bachelor of Science in Computer Science</strong>
+                            <span className="arrow-icon"> →</span> 
+                        </a>
                     </p>
                 </div>
                 <div>
-                    <p className="education-description">
-                        <strong>Schulich Leader Scholarship of Canada</strong>
+                <p className="education-description">
+                        <a href={schulichLink} target="_blank" rel="noopener noreferrer" className="scholarship-link">
+                            <strong>Schulich Leader Scholarship of Canada</strong>
+                            <span className="arrow-icon"> →</span>
+                        </a>
                     </p>
                     <p className="subdescription">
                         Awarded Canada’s most prestigious STEM scholarship valued at <strong>$100,000</strong> for academic excellence, leadership, and entrepreneurship.
@@ -24,7 +35,10 @@ const EducationComponent = () => {
                 </div>
                 <div>
                     <p className="education-description">
-                        <strong>Rajan Family Scholarship in Computing Science</strong>
+                    <a href={rajanLink} target="_blank" rel="noopener noreferrer" className="scholarship-link">
+                            <strong>Rajan Family Entrance Scholarship</strong>
+                            <span className="arrow-icon"> →</span>
+                        </a>
                     </p>
                     <p className="subdescription">
                         Offered a <strong>$40,000</strong> scholarship as the top female candidate entering SFU’s Computer Science program.
@@ -32,10 +46,13 @@ const EducationComponent = () => {
                 </div>
                 <div>
                     <p className="education-description">
-                        <strong>Governor General Bronze Medal of Canada</strong>
+                    <a href={gradMedal} target="_blank" rel="noopener noreferrer" className="scholarship-link">
+                            <strong>St. Patrick Regional Graduation Medal</strong>
+                            <span className="arrow-icon"> →</span>
+                        </a>
                     </p>
                     <p className="subdescription">
-                        Recognized as the top graduating student at the bronze level (99% average).
+                        Recognized as the top graduating student at St. Patrick Regional Secondary (99% average).
                     </p>
                 </div>
             </div>
